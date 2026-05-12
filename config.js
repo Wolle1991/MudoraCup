d307e403d665234e71131cc5e3a00dfb1cd8ff89
Wolle1991 <robin.prinz1@gmx.de>
2026-05-12 12:09:39 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Hier trägst du NUR deine Google Apps Script Web-App URL ein.
 // Wichtig: Das ist die /exec URL, NICHT die Bearbeitungs-URL aus Apps Script.
 window.MUDORA_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/DEINE_SCRIPT_ID/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyRhZukaIKyeJGxZeyuxBdJlhe2eLlrJEY8ZzOjP95lg2DWdDb3IH6vS2HlLwGmjMHL/exec"
 };
